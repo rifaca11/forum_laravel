9652856731f01e3c378b56baf6231be3c301c456
charifa <charifah1112@gmail.com>
2022-04-14 21:46:44 +0000

    first commit

---
+++ test_laravel/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+         //    validation
+         $this->validate($request, [
+            'email'=> 'required|email|max:255',
+            'password'=> 'required'
+        ]); 
+
+       if (!auth()-> attempt($request->only('email','password')))
+       {
+           return back()->with('status','invalid login details');
+       }
+       //    redirect
+       return redirect()->route('dashboard');
+
+
+    }
+}
---
+++ test_laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brief7</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-200">
+    <div>
+    <ul>
+        <nav class="p-6 bg-white flex justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
+                </li>
+                <li> 
+                    <a href="" class="p-3">Post</a>
+                </li>
+            </ul>
+
+
+            <ul class="flex items-center">
+                @auth
+                <li>
+                    <a href="" class="p-3">{{auth()->user()->name}}</a>
+                </li>
+
+                <li>
+                    <form action="{{ route('logout') }}" method="post" class="inline p-3">
+                        @csrf
+                    <button type="submit" class="p-3">Logout</a>
+                    </form>
+                </li>
+
+                @endauth
+
+                @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Registre</a>
+                </li>
+                @endguest
+
+                
+            </ul>
+
+        </nav>
+    </ul>
+    </div>
+   <!-- to extends this layout to other views -->
+    @yield('content')
+</body>
+</html>
